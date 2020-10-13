fafa8766a79528ca9cbb048ffedd7ec5482fecda
no1xsyzy <mf20070535@126.com>
2020-10-13 18:41:27 +0800

    style(storage/base): matches StandardJS

--- src/storage/base.js
+++ src/storage/base.js
@@ -1,14 +1,14 @@
-export function defineStorage( name, default_value ) {
-  if ( !( name in localStorage ) ) {
-    localStorage.setItem( name, JSON.stringify( default_value ) );
+export function defineStorage (name, defaultValue) {
+  if (!(name in localStorage)) {
+    localStorage.setItem(name, JSON.stringify(defaultValue))
   }
 
-  function getter() {
-    return JSON.parse( localStorage.getItem( name ) );
+  function getter () {
+    return JSON.parse(localStorage.getItem(name))
   }
 
-  function setter( v ) {
-    localStorage.setItem( name, JSON.stringify( v ) );
+  function setter (v) {
+    localStorage.setItem(name, JSON.stringify(v))
   }
-  return [ getter, setter ];
+  return [getter, setter]
 }
